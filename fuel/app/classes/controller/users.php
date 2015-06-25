1719ec6656238b04ac88e59083748c5856eb3529
Jessica Straw <jcstraw3@gmail.com>
2015-06-24 20:04:04 -0400

    Commit

--- fuel/app/classes/controller/users.php
+++ fuel/app/classes/controller/users.php
@@ -3,15 +3,26 @@
 class Controller_Users extends Controller_Template
 {
 
+// Register a user.
+
+	// Get register form and show to user.
+
 	public function get_register($fieldset = null, $errors = null)
 	{
+
+		// Init auth; pass view to variable.
+
 		$auth = Auth::instance();
 		$view = View::forge('users/register');
+
+		// Populate empty form from model.
 
 		if(empty($fieldset)){
 			$fieldset = Fieldset::forge('register');
 			Model_User::populate_register_fieldset($fieldset);
 		}
+
+		// Return view.
 
 		$view->set('reg', $fieldset->build(), false);
 		if($errors) $view->set_safe('errors', $errors);
@@ -19,18 +30,29 @@ class Controller_Users extends Controller_Template
 		$this->template->content = $view;
 	}
 
+	// Post register form data to database.
+
 	public function post_register(){
+
+		// Grab populated form and pass into a variable.
+
 		$fieldset = Model_User::populate_register_fieldset(Fieldset::forge('register'));
 		$fieldset->repopulate();
 		$result = Model_User::validate_registration($fieldset, Auth::instance());
+
+		// Show error.s
 
 		if($result['e_found']){
 			return $this->get_register($fieldset, $result['errors']);
 		}
 
+		// Redirect.
+
 		Session::set_flash('success', 'User created.');
 		Response::redirect('notes');
 	}
+
+// Log in a user.
 
 	public function action_login()
 	{
@@ -38,6 +60,8 @@ class Controller_Users extends Controller_Template
 		$this->template->title = 'Users &raquo; Login';
 		$this->template->content = View::forge('users/login', $data);
 	}
+
+// Log out a user.
 
 	public function action_logout()
 	{
