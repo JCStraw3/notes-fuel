805b20fc5cc4b164c93453f7b58087907e795497
Jessica Straw <jcstraw3@gmail.com>
2016-01-24 22:11:38 -0500

    Adds comments to user model

--- fuel/app/classes/model/user.php
+++ fuel/app/classes/model/user.php
@@ -4,6 +4,8 @@ use Orm\Model;
 
 class Model_User extends Model {
 
+	// Attributes.
+
 	protected static $_properties = array(
 		'id',
 		'name',
@@ -16,6 +18,8 @@ class Model_User extends Model {
 		'updated_at',
 	);
 
+	// Timestamps.
+
 	protected static $_observers = array(
 		'Orm\Observer_CreatedAt' => array(
 			'events' => array('before_insert'),
@@ -27,7 +31,11 @@ class Model_User extends Model {
 		),
 	);
 
+	// Table name.
+
 	protected static $_table_name = 'users';
+
+	// Creates the registration form.
 
 	public static function populate_register_fieldset(Fieldset $form){
 		$form->add('name', 'Name:')->add_rule('required');
@@ -38,6 +46,8 @@ class Model_User extends Model {
 		return $form;
 	}
 
+	// Takes in the data from the registration form.
+
 	public static function register(Fieldset $form){
 		$form->add('name', 'Name:')->add_rule('required');
 		$form->add('email', 'Email:')->add_rule('valid_email');
@@ -47,14 +57,23 @@ class Model_User extends Model {
 		return $form;
 	}
 
+	// Validates registration data and saves to database.
+
 	public static function validate_registration(Fieldset $form, $auth){
+
+		// Checks to see it the passwords match.
+
 		$form->field('password')->add_rule('match_value', $form->field('password2')->get_attribute('value'));
+
+		// Validates the inputed data.
 
 		$val = $form->validation();
 		$val->set_message('required', ':field is a required field');
 		$val->set_message('valid_email', ':field must be a valid email');
 		$val->set_message('match_value', 'The passwords must match');
 
+		// Saves data to database or throws error.
+		
 		if($val->run()){
 			$name = $form->field('name')->get_attribute('value');
 			$email = $form->field('email')->get_attribute('value');
