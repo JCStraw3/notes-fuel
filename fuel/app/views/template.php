6540121550c4b2b064adb306da81173073ae26a4
Jessica Straw <jcstraw3@gmail.com>
2016-01-25 07:10:46 -0500

    Moves the css file to proper folder, edits the style link in view template file

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -5,25 +5,7 @@
 	<meta charset="utf-8">
 	<title><?php echo $title; ?></title>
 	<?php echo Asset::css('bootstrap.css'); ?>
-	<style>
-		body { margin: 40px; }
-		.note{
-			background: #FFFF80;
-			width: 500px;
-			margin: 20px;
-			padding: 1px;
-			padding-left: 20px;
-		}
-		.nav{
-			background: #C0C0C0;
-			color: white;
-			width: 100%;
-			padding: 10px;
-		}
-		a{
-			color: #84FF09;
-		}
-	</style>
+	<?php echo Asset::css('app.css'); ?>
 </head>
 
 <body>
