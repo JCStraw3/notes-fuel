2c3b487b3084483d85dac6570cee7193637e74ac
Jessica Straw <jcstraw3@gmail.com>
2016-01-30 11:07:00 -0500

    Commit

--- fuel/app/views/welcome/404.php
+++ fuel/app/views/welcome/404.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
+	<meta charset='utf-8'>
 	<title><?php echo $title; ?></title>
 	<?php echo Asset::css('bootstrap.css'); ?>
 	<style>
@@ -31,13 +31,13 @@
 </head>
 <body>
 	<header>
-		<div class="container">
-			<div id="logo"></div>
+		<div class='container'>
+			<div id='logo'></div>
 		</div>
 	</header>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
+	<div class='container'>
+		<div class='row'>
+			<div class='col-md-12'>
 				<h1><?php echo $title; ?> <small>We can't find that!</small></h1>
 				<hr>
 				<p>The controller generating this page is found at <code>APPPATH/classes/controller/welcome.php</code>.</p>
@@ -45,9 +45,9 @@
 			</div>
 		</div>
 		<footer>
-			<p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
+			<p class='pull-right'>Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
 			<p>
-				<a href="http://fuelphp.com">FuelPHP</a> is released under the MIT license.<br>
+				<a href='http://fuelphp.com'>FuelPHP</a> is released under the MIT license.<br>
 				<small>Version: <?php echo Fuel::VERSION; ?></small>
 			</p>
 		</footer>
